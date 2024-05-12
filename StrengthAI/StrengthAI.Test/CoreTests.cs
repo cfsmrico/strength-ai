ca2f137b19cc90b3806591fcdcdde87558aef76b
Chaney <chaneyfonseca@gmail.com>
2024-05-12 17:45:10 -0500

    Add two more failer tests

--- StrengthAI/StrengthAI.Test/CoreTests.cs
+++ StrengthAI/StrengthAI.Test/CoreTests.cs
@@ -21,5 +21,35 @@ namespace StrengthAI.Test
             var output = TDEE.GetEstimate(input);
             Assert.IsTrue(output == 3802);
         }
+
+        public void Two()
+        {
+            var input = new InputSet
+            {
+                Age = 42,
+                ActivityLevel = ActivityLevel.ModeratelyActive,
+                BodyWeight = 188,
+                HeightInCentimeters = 181,
+                BodyFatPercentage = 12
+            };
+
+            var output = TDEE.GetEstimate(input);
+            Assert.IsTrue(output == 3683);
+        }
+
+        public void Three()
+        {
+            var input = new InputSet
+            {
+                Age = 43,
+                ActivityLevel = ActivityLevel.Average,
+                BodyWeight = 200,
+                HeightInCentimeters = 181,
+                BodyFatPercentage = 15
+            };
+
+            var output = TDEE.GetEstimate(input);
+            Assert.IsTrue(output == 3257);
+        }
     }
 }
